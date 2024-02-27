52e6966acadf79bb3639a15b2877d77d1e6d35e8
Michael Tupek <tupek2@llnl.gov>
2024-02-27 14:54:43 -0800

    Refactor domain decomp stuff into a library.  Could still be cleaner.

--- examples/aggregation/test_quilt.py
+++ examples/aggregation/test_quilt.py
@@ -1,6 +1,5 @@
 # jaxy stuff
 from collections import namedtuple
-import numpy as onp
 import jax
 import jax.numpy as np
 # data class
@@ -13,15 +12,17 @@ from optimism.Timer import timeme
 from Plotting import output_mesh_and_fields
 
 # poly stuff
-from optimism.aggregation import Coarsening
+
+from optimism.aggregation import DomainDecomp
 
 # mesh stuff
-from optimism import ReadExodusMesh
+#from optimism import ReadExodusMesh
+#from optimism import Mesh
 from optimism.FunctionSpace import EssentialBC
 from optimism.FunctionSpace import DofManager
 
 # physics stuff
-import optimism.TensorMath as TensorMath
+
 import optimism.QuadratureRule as QuadratureRule
 import optimism.FunctionSpace as FunctionSpace
 from optimism.material import Neohookean as MatModel
@@ -36,20 +37,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '/Users/tupek2/dev/agglo
 import quilts
 
 DIRICHLET_INDEX = np.iinfo(np.int32).min
-
-def tet_quadrature_grad(field, shapeGrad, neighbors):
-    return np.tensordot(field[neighbors], shapeGrad, axes=[0,0])
-
-
-def tet_quadrature_energy(field, stateVars, shapeGrad, neighbors, material):
-    gradU = tet_quadrature_grad(field, shapeGrad, neighbors)
-    gradU3x3 = TensorMath.tensor_2D_to_3D(gradU)
-    energyDensity = material.compute_energy_density(gradU3x3, np.array([0]), stateVars)
-    return energyDensity
-
-
-def poly_subtet_energy(field, stateVars, B, vols, conns, material):
-    return vols @ jax.vmap(tet_quadrature_energy, (None,0,0,None,None))(field, stateVars, B, conns, material)
 
 
 trSettings = EquationSolver.get_settings(max_trust_iters=400, t1=0.4, t2=1.5, eta1=1e-6, eta2=0.2, eta3=0.8)
@@ -80,11 +67,6 @@ class PolyPatchTest(MeshFixture.MeshFixture):
         self.expectedVolume = (xRange[1]-xRange[0]) * (yRange[1]-yRange[0])
         self.mesh, self.dispTarget = self.create_mesh_and_disp(self.Nx, self.Ny, xRange, yRange, lambda x : self.targetDispGrad.T@x)
 
-        #self.mesh = ReadExodusMesh.read_exodus_mesh('crack_domain.g')
-
-        self.quadRule = QuadratureRule.create_quadrature_rule_on_triangle(degree=1)
-        self.fs = FunctionSpace.construct_function_space(self.mesh, self.quadRule)
-
         kappa = 10.0
         nu = 0.3
         E = 3*kappa*(1 - 2*nu)
@@ -93,6 +75,10 @@ class PolyPatchTest(MeshFixture.MeshFixture):
         props = {'elastic modulus': E,
                  'poisson ratio': nu,
                  'version': 'coupled'}
+
+
+        self.quadRule = QuadratureRule.create_quadrature_rule_on_triangle(degree=1)
+        self.fs = FunctionSpace.construct_function_space(self.mesh, self.quadRule)
 
         self.setup='bending' # dirichlet_patch, neumann_patch
         #self.setup='neumann_patch' # dirichlet_patch, neumann_patch
@@ -135,15 +121,6 @@ class PolyPatchTest(MeshFixture.MeshFixture):
         self.internals = mcxFuncs.compute_initial_state()
 
 
-    def fine_poly_energy(self, polyDisp, U, stateVars, tetElemsInPoly, fineNodesInPoly):
-        U = U.at[fineNodesInPoly[::-1]].set(polyDisp[::-1])  # reverse nodes so first node in conns list so first actual appearence of it is used in the index update
-        shapeGrads = self.fs.shapeGrads[tetElemsInPoly]
-        vols = self.fs.vols[tetElemsInPoly]
-        conns = self.mesh.conns[tetElemsInPoly]
-        stateVarsE = stateVars[tetElemsInPoly]
-        energyDensities = jax.vmap(poly_subtet_energy, (None,0,0,0,0,None))(U, stateVarsE, shapeGrads, vols, conns, self.materialModel)
-        return np.sum(energyDensities)
-        
 
     def test_poly_patch_test_all_dirichlet(self):
         # MRT eventually use the dirichlet ones to precompute some initial strain offsets/biases
@@ -167,59 +144,29 @@ class PolyPatchTest(MeshFixture.MeshFixture):
 
         U = dofManager.create_field(0.0 * dofManager.get_unknown_values(self.dispTarget), dofManager.get_bc_values(self.dispTarget))
 
-        partitionElemField, activeNodes, polyElems, polyNodes, dofStatus = self.construct_aggregations(dofManager, dirichletSets)
+        partitionElemField, activeNodes, polyElems, polyNodes, dofStatus = DomainDecomp.construct_aggregations(self.mesh, self.numParts, dofManager, dirichletSets)
 
-        poly_energy = lambda pU, pNodes, pElems : self.fine_poly_energy(pU, U, self.internals, pElems, pNodes)
+        poly_energy = lambda pU, pNodes, pElems : DomainDecomp.fine_poly_energy(self.mesh, self.fs, self.materialModel, pU, U, self.internals, pElems, pNodes)
         poly_stiffness = jax.jit(jax.hessian(poly_energy,0))
 
         allBoundaryInCoarse = False
-        #allBoundaryInCoarse = True
-        if allBoundaryInCoarse:
-            dofStatus = np.where(dofStatus==-2, 1, dofStatus)
-            wherePos = dofStatus>=0.0
-            numActive = np.count_nonzero(wherePos)
-            dofStatus = dofStatus.at[wherePos].set(np.arange(numActive))
+        noInteriorDofs = False
+        useQuilt = True
 
-        noInterior = False
-        if noInterior:
-            dofStatus = np.where(dofStatus==-1, 1, dofStatus)
-            wherePos = dofStatus>=0.0
-            numActive = np.count_nonzero(wherePos)
-            dofStatus = dofStatus.at[wherePos].set(np.arange(numActive))
-
-        useQuilt = False
-        if useQuilt:
-            linOp = quilts.QuiltOperatorSym(dofStatus, DIRICHLET_INDEX)
-            for pNodes, pElems in zip(polyNodes, polyElems):
-                pU = U[pNodes]
-                nDofs = pU.size
-                polyDofs = np.stack((2*pNodes,2*pNodes+1), axis=1).ravel()
-                stiff_pp = poly_stiffness(pU, pNodes, pElems).reshape(nDofs,nDofs)
-                polyX = self.mesh.coords[pNodes,0]; polyX = np.stack((polyX, polyX), axis=1).ravel()
-                polyY = self.mesh.coords[pNodes,1]; polyY = np.stack((polyY, polyY), axis=1).ravel()
-                linOp.add_poly(polyDofs, stiff_pp, polyX, polyY)
-            linOp.finalize()
-        else:
-            linOp = quilts.BddcOperatorSym(dofStatus, True, DIRICHLET_INDEX)
-            for pNodes, pElems in zip(polyNodes, polyElems):
-                pU = U[pNodes]
-                nDofs = pU.size
-                pStiffness = poly_stiffness(pU, pNodes, pElems).reshape(nDofs,nDofs)
-                polyDofs = np.stack((2*pNodes,2*pNodes+1), axis=1).ravel()
-                linOp.add_poly(polyDofs, pStiffness)
-            linOp.finalize()
+        linOp = DomainDecomp.create_linear_operator(self.mesh, U, polyElems, polyNodes, poly_stiffness, dofStatus, allBoundaryInCoarse, noInteriorDofs, useQuilt)
 
         def energy(Ut):
-            return  self.compute_energy(Ut, self.internals) + self.external_energy_function(Ut)
+            return self.compute_energy(Ut, self.internals) + self.external_energy_function(Ut)
 
         def residual(Ut):
             g = self.compute_gradient(Ut, self.internals).ravel() + self.load_function(Ut).ravel()
             g = np.where(dofStatus.reshape(g.shape) < -100, 0.0, g)
             return g
         
-        precondMethod = quilts.TrustRegionSettings.BDDC_AND_DIAGONAL
-        #precondMethod = quilts.TrustRegionSettings.BDDC
-        #precondMethod = quilts.TrustRegionSettings.DIAGONAL
+        precondMethod = quilts.TrustRegionSettings.COARSE_JACOBI_COARSE
+        #precondMethod = quilts.TrustRegionSettings.JACOBI_COARSE_JACOBI
+        #precondMethod = quilts.TrustRegionSettings.JACOBI
+
         cgTolerance = 1e-10
         trTolerance = 2.5 * cgTolerance
         maxCgIters = 50
@@ -229,7 +176,7 @@ class PolyPatchTest(MeshFixture.MeshFixture):
 
         delta = 1.0 #1e-4 # 100
 
-        U = solve_nonlinear_problem(U, polyElems, polyNodes, poly_stiffness, linOp, energy, residual, trTolerance, cgSettings, boundaryTolerance, delta)
+        U = DomainDecomp.solve_nonlinear_problem(U, polyElems, polyNodes, poly_stiffness, linOp, energy, residual, trSettings, trTolerance, cgSettings, boundaryTolerance, delta)
              
         dofStatus = dofStatus.reshape(U.shape) # readable dofstatus
         output_mesh_and_fields('patch', self.mesh, 
@@ -239,175 +186,38 @@ class PolyPatchTest(MeshFixture.MeshFixture):
                                                     ('dof_status', dofStatus)])
 
 
-    def set_dirichlet(self, stiff, whereDirichlet):
-        for id, isDir in enumerate(whereDirichlet):
-            if isDir:
-                stiff = stiff.at[:,id].set(0.0)
-                stiff = stiff.at[id,:].set(0.0)
-                stiff = stiff.at[id,id].set(1.0)
-        return stiff
+    #def set_dirichlet(self, stiff, whereDirichlet):
+    #    for id, isDir in enumerate(whereDirichlet):
+    #        if isDir:
+    #            stiff = stiff.at[:,id].set(0.0)
+    #            stiff = stiff.at[id,:].set(0.0)
+    #            stiff = stiff.at[id,id].set(1.0)
+    #    return stiff
 
-    def set_residual_dirichlet(self, r, whereDirichlet):
-        for id, isDir in enumerate(whereDirichlet):
-            if isDir:
-                r = r.at[id].set(0.0)
-        return r
+    #def set_residual_dirichlet(self, r, whereDirichlet):
+    #    for id, isDir in enumerate(whereDirichlet):
+    #        if isDir:
+    #            r = r.at[id].set(0.0)
+    #    return r
 
-    def create_interpolation(self, nDofs):
-        dofs_c = np.hstack((np.arange(12),np.arange(14,nDofs)))
-        dofs_f = np.array([12,13])
+    #def create_interpolation(self, nDofs):
+    #    dofs_c = np.hstack((np.arange(12),np.arange(14,nDofs)))
+    #    dofs_f = np.array([12,13])
 
-        # removing 2 dofs 
-        interp = np.zeros((nDofs,nDofs-2))
-        for i in range(12):
-            interp = interp.at[i,i].set(1.0)
-        interp = interp.at[12,2].set(1.0)
-        interp = interp.at[12,10].set(-1.0)
-        interp = interp.at[12,18].set(1.0)
-        interp = interp.at[13,11].set(-1.0)
-        interp = interp.at[13,19].set(1.0)
-        for i in range(14,nDofs):
-            interp = interp.at[i,i-2].set(1.0)
-        return dofs_c,dofs_f,interp
-
-
-    def construct_aggregations(self, dofManager, dirichletSets):
-        partitionElemField, polyElems, polyNodes, nodesToColors, activeNodes = \
-          self.construct_aggregates(self.numParts, ['bottom','top','right','left'], dirichletSets)
-
-        polyNodes = [np.array(list(polyNodes[index]), dtype=np.int64) for index in polyNodes]
-        polyElems = [np.array(list(polyElems[index]), dtype=np.int64) for index in polyElems]
-
-        colorCount = np.array([len(nodesToColors[s]) for s in range(len(activeNodes))])
-        isActive = activeNodes==1
-
-        nodeStatus = -np.ones_like(activeNodes, dtype=np.int64)
-        whereActive = np.where(isActive)[0]
-        nodeStatus = nodeStatus.at[whereActive].set(0)
-
-        whereInactive = np.where(~isActive)[0]
-        self.assertTrue( np.max(colorCount[whereInactive]) < 3 ) # because we are 2D for now
-        nodeStatus = nodeStatus.at[whereInactive].set(colorCount[whereInactive]*nodeStatus[whereInactive])
-
-        dofStatus = np.stack((nodeStatus,nodeStatus), axis=1)
-        dofStatus = dofStatus.at[dofManager.isBc].set(DIRICHLET_INDEX).ravel()
-  
-        whereGlobal = np.where(dofStatus>=0)[0]
-        #print('num global dofs = ', whereGlobal)
-        dofStatus = dofStatus.at[whereGlobal].set(np.arange(len(whereGlobal)))
-
-        return partitionElemField,activeNodes,polyElems,polyNodes,dofStatus
+    #    # removing 2 dofs 
+    #    interp = np.zeros((nDofs,nDofs-2))
+    #    for i in range(12):
+    #        interp = interp.at[i,i].set(1.0)
+    #    interp = interp.at[12,2].set(1.0)
+    #    interp = interp.at[12,10].set(-1.0)
+    #    interp = interp.at[12,18].set(1.0)
+    #    interp = interp.at[13,11].set(-1.0)
+    #    interp = interp.at[13,19].set(1.0)
+    #    for i in range(14,nDofs):
+    #        interp = interp.at[i,i-2].set(1.0)
+    #    return dofs_c,dofs_f,interp
 
 
-    # geometric boundaries must cover the entire boundary.
-    # coarse nodes are maintained wherever the node is involved in 2 boundaries
-    @timeme
-    def construct_aggregates(self, numParts, geometricBoundaries, dirichletBoundaries):
-        partitionElemField = Coarsening.create_partitions(self.mesh.conns, numParts)
-        polyElems = Coarsening.create_poly_elems(partitionElemField) # dict from poly number to elem numbers
-        polyNodes = Coarsening.extract_poly_nodes(self.mesh.conns, polyElems) # dict from poly number to global node numbers
-
-        nodesToColors = Coarsening.create_nodes_to_colors(self.mesh.conns, partitionElemField)
-
-        approximationBoundaries = geometricBoundaries.copy()
-        for b in dirichletBoundaries: approximationBoundaries.append(b)
-
-        nodesToBoundary = Coarsening.create_nodes_to_boundaries(self.mesh, approximationBoundaries)
-
-        activeNodes = onp.zeros_like(self.mesh.coords)[:,0]
-        Coarsening.activate_nodes(nodesToColors, nodesToBoundary, activeNodes)
-
-        for p in polyNodes:
-          nodesOfPoly = polyNodes[p]
-          polyFaces, polyExterior, polyInterior = Coarsening.divide_poly_nodes_into_faces_and_interior(nodesToBoundary, nodesToColors, p, nodesOfPoly)
-
-          for f in polyFaces:
-              faceNodes = onp.array(list(polyFaces[f]))
-              # warning, this next function modifies activeNodes
-              active, inactive, lengthScale = Coarsening.determine_active_and_inactive_face_nodes(faceNodes, self.mesh.coords, activeNodes, True)
-
-        return partitionElemField, polyElems, polyNodes, nodesToColors, np.array(activeNodes)
-    
-
-
-def solve_nonlinear_problem(U, polyElems, polyNodes, poly_stiffness, linOp, energy, residual, trTolerance, cgSettings, boundaryTolerance, delta):
-    logfile = 'log.txt'
-    f = open(logfile, "w")
-
-    maxCgItersToResetPrecond = 35
-
-    # propagate dirichlet bc info to stitch dofs
-    Ushp = U.shape
-    U = linOp.set_dirichlet_values(U.ravel()).reshape(Ushp)
-    dU = 0.0*U.ravel()
-
-    g = residual(U)
-    leftMost = g.copy()
-
-    gNorm = np.linalg.norm(g)
-
-    for trustIter in range(trSettings.max_trust_iters):
-        f.write('delta = ' + str(delta)  + ' at iter ' + str(trustIter) + '\n')
-        energyBase = energy(U)
-
-        f.close()
-        modelEnergyChange, trustIters, isOnBoundary = quilts.solve_trust_region_model_problem(cgSettings, linOp, g, delta, leftMost, dU)
-        f = open(logfile, "a")
-
-        f.write('trust region step norm = ' + str(np.linalg.norm(dU)) + ' after ' + str(trustIters) + ' cg iterations\n')
-        UTrial = U + dU.reshape(U.shape)
-        gTrial = residual(UTrial)
-
-        #if settings.use_incremental_objective:
-        #incrementalEnergyChange = 0.5 * (dU @ (g+gTrial))
-        energyTrial = energy(UTrial)
-        incrementalEnergyChange = energyTrial - energyBase
-
-        f.write('model vs real changes = ' + str(modelEnergyChange) + ' ' + str(incrementalEnergyChange) + '\n')
-
-        rho = incrementalEnergyChange / modelEnergyChange
-
-        if modelEnergyChange > 0:
-            f.write('error: Found a positive model objective increase.  Debug if you see this.\n')
-            rho = -rho
-            
-        if not rho >= trSettings.eta2:  # write it this way to handle NaNs
-            delta *= trSettings.t1
-        elif rho > trSettings.eta3 and np.linalg.norm(dU) > boundaryTolerance * delta:
-            delta *= trSettings.t2
-
-        gTrialNorm = np.linalg.norm(gTrial)
-
-        f.write('residual norm = ' + str(gTrialNorm) + '\n')
-
-        willAccept = rho >= trSettings.eta1 or (rho >= -0 and gTrialNorm <= gNorm)
-        if willAccept:
-            print("accepting trust region solve after", trustIters, "iterations.")
-            f.write('accepting trust region step\n\n')
-            g = gTrial
-            gNorm = gTrialNorm
-            U = UTrial
-            if gNorm <= trTolerance:
-                print('converged nonlinear problem\n\n')
-                break
-
-            for iPoly, (pNodes, pElems) in enumerate(zip(polyNodes, polyElems)):
-                pU = U[pNodes]
-                nDofs = pU.size
-                stiff_pp = poly_stiffness(pU, pNodes, pElems).reshape(nDofs,nDofs)
-                linOp.update_poly_stiffness(iPoly , stiff_pp)
-
-        else:
-            f.write('rejecting trust region step ' + str(rho) + '\n\n')
-
-        if trustIters > maxCgItersToResetPrecond:
-            print('updating preconditioner')
-            f.close()
-            linOp.update_preconditioner()
-            f = open(logfile, "a")
-
-    f.close()
-    return U
 
 
 if __name__ == '__main__':
---
+++ optimism/aggregation/DomainDecomp.py
@@ -0,0 +1,207 @@
+import jax
+import jax.numpy as np
+import numpy as onp
+import optimism.TensorMath as TensorMath
+from optimism.aggregation import Coarsening
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '/Users/tupek2/dev/agglomerationpreconditioner/python'))
+import quilts
+
+DIRICHLET_INDEX = np.iinfo(np.int32).min
+
+def tet_quadrature_field_grad(field, shapeGrad, neighbors):
+    return np.tensordot(field[neighbors], shapeGrad, axes=[0,0])
+
+def tet_quadrature_energy(field, stateVars, shapeGrad, neighbors, material):
+    gradU = tet_quadrature_field_grad(field, shapeGrad, neighbors)
+    gradU3x3 = TensorMath.tensor_2D_to_3D(gradU)
+    energyDensity = material.compute_energy_density(gradU3x3, np.array([0]), stateVars)
+    return energyDensity
+
+def poly_subtet_energy(field, stateVars, B, vols, conns, material):
+    return vols @ jax.vmap(tet_quadrature_energy, (None,0,0,None,None))(field, stateVars, B, conns, material)
+
+def fine_poly_energy(mesh, fs, materialModel, polyDisp, U, stateVars, tetElemsInPoly, fineNodesInPoly):
+    U = U.at[fineNodesInPoly[::-1]].set(polyDisp[::-1])  # reverse nodes so first node in conns list so first actual appearence of it is used in the index update
+    shapeGrads = fs.shapeGrads[tetElemsInPoly]
+    vols = fs.vols[tetElemsInPoly]
+    conns = mesh.conns[tetElemsInPoly]
+    stateVarsE = stateVars[tetElemsInPoly]
+    energyDensities = jax.vmap(poly_subtet_energy, (None,0,0,0,0,None))(U, stateVarsE, shapeGrads, vols, conns, materialModel)
+    return np.sum(energyDensities)
+
+
+# geometric boundaries must cover the entire boundary.
+# coarse nodes are maintained wherever the node is involved in 2 boundaries
+def construct_aggregates(mesh, numParts, geometricBoundaries, dirichletBoundaries):
+    partitionElemField = Coarsening.create_partitions(mesh.conns, numParts)
+    polyElems = Coarsening.create_poly_elems(partitionElemField) # dict from poly number to elem numbers
+    polyNodes = Coarsening.extract_poly_nodes(mesh.conns, polyElems) # dict from poly number to global node numbers
+
+    nodesToColors = Coarsening.create_nodes_to_colors(mesh.conns, partitionElemField)
+
+    approximationBoundaries = geometricBoundaries.copy()
+    for b in dirichletBoundaries: approximationBoundaries.append(b)
+
+    nodesToBoundary = Coarsening.create_nodes_to_boundaries(mesh, approximationBoundaries)
+
+    activeNodes = onp.zeros_like(mesh.coords)[:,0]
+    Coarsening.activate_nodes(nodesToColors, nodesToBoundary, activeNodes)
+
+    for p in polyNodes:
+        nodesOfPoly = polyNodes[p]
+        polyFaces, polyExterior, polyInterior = Coarsening.divide_poly_nodes_into_faces_and_interior(nodesToBoundary, nodesToColors, p, nodesOfPoly)
+
+        for f in polyFaces:
+            faceNodes = onp.array(list(polyFaces[f]))
+            # warning, this next function modifies activeNodes
+            active, inactive, lengthScale = Coarsening.determine_active_and_inactive_face_nodes(faceNodes, mesh.coords, activeNodes, True)
+
+    return partitionElemField, polyElems, polyNodes, nodesToColors, np.array(activeNodes)
+
+
+def construct_aggregations(mesh, numParts, dofManager, dirichletSets):
+    partitionElemField, polyElems, polyNodes, nodesToColors, activeNodes = \
+        construct_aggregates(mesh, numParts, ['bottom','top','right','left'], dirichletSets)
+
+    polyNodes = [np.array(list(polyNodes[index]), dtype=np.int64) for index in polyNodes]
+    polyElems = [np.array(list(polyElems[index]), dtype=np.int64) for index in polyElems]
+
+    colorCount = np.array([len(nodesToColors[s]) for s in range(len(activeNodes))])
+    isActive = activeNodes==1
+
+    nodeStatus = -np.ones_like(activeNodes, dtype=np.int64)
+    whereActive = np.where(isActive)[0]
+    nodeStatus = nodeStatus.at[whereActive].set(0)
+
+    whereInactive = np.where(~isActive)[0]
+    #self.assertTrue( np.max(colorCount[whereInactive]) < 3 ) # because we are 2D for now
+    nodeStatus = nodeStatus.at[whereInactive].set(colorCount[whereInactive]*nodeStatus[whereInactive])
+
+    dofStatus = np.stack((nodeStatus,nodeStatus), axis=1)
+    dofStatus = dofStatus.at[dofManager.isBc].set(DIRICHLET_INDEX).ravel()
+
+    whereGlobal = np.where(dofStatus>=0)[0]
+    #print('num global dofs = ', whereGlobal)
+    dofStatus = dofStatus.at[whereGlobal].set(np.arange(len(whereGlobal)))
+
+    return partitionElemField,activeNodes,polyElems,polyNodes,dofStatus
+
+def create_linear_operator(mesh, U, polyElems, polyNodes, poly_stiffness_func, dofStatus, allBoundaryInCoarse, noInteriorDofs, useQuilt):
+    if allBoundaryInCoarse:
+        dofStatus = np.where(dofStatus==-2, 1, dofStatus)
+        wherePos = dofStatus>=0.0
+        numActive = np.count_nonzero(wherePos)
+        dofStatus = dofStatus.at[wherePos].set(np.arange(numActive))
+
+    if noInteriorDofs:
+        dofStatus = np.where(dofStatus==-1, 1, dofStatus)
+        wherePos = dofStatus>=0.0
+        numActive = np.count_nonzero(wherePos)
+        dofStatus = dofStatus.at[wherePos].set(np.arange(numActive))
+
+    if useQuilt:
+        linOp = quilts.QuiltOperatorSym(dofStatus, DIRICHLET_INDEX)
+        for pNodes, pElems in zip(polyNodes, polyElems):
+            pU = U[pNodes]
+            nDofs = pU.size
+            polyDofs = np.stack((2*pNodes,2*pNodes+1), axis=1).ravel()
+            stiff_pp = poly_stiffness_func(pU, pNodes, pElems).reshape(nDofs,nDofs)
+            polyX = mesh.coords[pNodes,0]; polyX = np.stack((polyX, polyX), axis=1).ravel()
+            polyY = mesh.coords[pNodes,1]; polyY = np.stack((polyY, polyY), axis=1).ravel()
+            linOp.add_poly(polyDofs, stiff_pp, polyX, polyY)
+        linOp.finalize()
+    else:
+        linOp = quilts.BddcOperatorSym(dofStatus, True, DIRICHLET_INDEX)
+        for pNodes, pElems in zip(polyNodes, polyElems):
+            pU = U[pNodes]
+            nDofs = pU.size
+            pStiffness = poly_stiffness_func(pU, pNodes, pElems).reshape(nDofs,nDofs)
+            polyDofs = np.stack((2*pNodes,2*pNodes+1), axis=1).ravel()
+            linOp.add_poly(polyDofs, pStiffness)
+        linOp.finalize()
+
+    return linOp
+
+
+def solve_nonlinear_problem(U, polyElems, polyNodes, poly_stiffness_func, linOp, energy_func, residual_func, trSettings, trTolerance, cgSettings, boundaryTolerance, delta):
+    logfile = 'log.txt'
+    f = open(logfile, "w")
+
+    maxCgItersToResetPrecond = 35
+
+    # propagate dirichlet bc info to stitch dofs
+    Ushp = U.shape
+    U = linOp.set_dirichlet_values(U.ravel()).reshape(Ushp)
+    dU = 0.0*U.ravel()
+
+    g = residual_func(U)
+    leftMost = g.copy()
+
+    gNorm = np.linalg.norm(g)
+
+    for trustIter in range(trSettings.max_trust_iters):
+        f.write('delta = ' + str(delta)  + ' at iter ' + str(trustIter) + '\n')
+        energyBase = energy_func(U)
+
+        f.close()
+        modelEnergyChange, trustIters, isOnBoundary = quilts.solve_trust_region_model_problem(cgSettings, linOp, g, delta, leftMost, dU)
+        f = open(logfile, "a")
+
+        f.write('trust region step norm = ' + str(np.linalg.norm(dU)) + ' after ' + str(trustIters) + ' cg iterations\n')
+        UTrial = U + dU.reshape(U.shape)
+        gTrial = residual_func(UTrial)
+
+        #if settings.use_incremental_objective:
+        #incrementalEnergyChange = 0.5 * (dU @ (g+gTrial))
+        energyTrial = energy_func(UTrial)
+        incrementalEnergyChange = energyTrial - energyBase
+
+        f.write('model vs real changes = ' + str(modelEnergyChange) + ' ' + str(incrementalEnergyChange) + '\n')
+
+        rho = incrementalEnergyChange / modelEnergyChange
+
+        if modelEnergyChange > 0:
+            f.write('error: Found a positive model objective increase.  Debug if you see this.\n')
+            rho = -rho
+            
+        if not rho >= trSettings.eta2:  # write it this way to handle NaNs
+            delta *= trSettings.t1
+        elif rho > trSettings.eta3 and np.linalg.norm(dU) > boundaryTolerance * delta:
+            delta *= trSettings.t2
+
+        gTrialNorm = np.linalg.norm(gTrial)
+
+        f.write('residual norm = ' + str(gTrialNorm) + '\n')
+
+        willAccept = rho >= trSettings.eta1 or (rho >= -0 and gTrialNorm <= gNorm)
+        if willAccept:
+            print("accepting trust region solve after", trustIters, "iterations.")
+            f.write('accepting trust region step\n\n')
+            g = gTrial
+            gNorm = gTrialNorm
+            U = UTrial
+            if gNorm <= trTolerance:
+                print('converged nonlinear problem\n\n')
+                break
+
+            for iPoly, (pNodes, pElems) in enumerate(zip(polyNodes, polyElems)):
+                pU = U[pNodes]
+                nDofs = pU.size
+                stiff_pp = poly_stiffness_func(pU, pNodes, pElems).reshape(nDofs,nDofs)
+                linOp.update_poly_stiffness(iPoly , stiff_pp)
+
+        else:
+            f.write('rejecting trust region step ' + str(rho) + '\n\n')
+
+        if trustIters > maxCgItersToResetPrecond:
+            print('updating preconditioner')
+            f.write('updating preconditioner')
+            f.close()
+            linOp.update_preconditioner()
+            f = open(logfile, "a")
+
+    f.close()
+    return U
